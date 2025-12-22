aa88583af5ac6ee5d6caa541e01ad80e390cca4d
randhavevaibhav <randhavevaibhav3@gmail.com>
2025-12-22 13:13:17 +0530

    file name correction

--- cypress/e2e/pages/auth/master.cy.ts
+++ cypress/e2e/pages/auth/master.cy.ts
@@ -1,2 +1,2 @@
 import "@Cypress/e2e/pages/auth/login-form/login-form-spec.cy";
-import "@Cypress/e2e/pages/auth/login-form/signup-form-spec.cy.ts";
+import "@Cypress/e2e/pages/auth/login-form/signup-form-spec.cy";
